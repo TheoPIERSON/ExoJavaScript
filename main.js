b58061f47d5dacaf86bb775c394fe27f32632963
Theo Pierson <thheopierson@gmail.com>
2023-06-02 12:41:25 +0200

    docs: finish exercice 2

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//////EXO 1
 let i = 0;
 while (++i < 5) alert(i);
 /* alert affiche:
@@ -15,4 +16,21 @@ while (j++ < 5) alert(j);
 - 3
 - 4
 - 5
+*/
+////// EXO 2
+for (let i = 0; i < 5; i++) alert(i);
+/* alert affiche:
+- 0
+- 1
+- 2
+- 3
+- 4
+*/
+for (let i = 0; i < 5; ++i) alert(i);
+/* alert affiche:
+- 0
+- 1
+- 2
+- 3
+- 4
 */
